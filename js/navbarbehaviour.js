56b4f5571beaa6b3f951acd6143175f85a61cfa7
mario rojas <mariomgmp10@gmail.com>
2025-03-27 04:48:43 +0100

    moruvidas

--- js/navbarbehaviour.js
+++ js/navbarbehaviour.js
@@ -15,3 +15,33 @@ if (currentPageUnderline){
     currentPageUnderline.parentElement.style.backgroundColor = "rgba(126, 126, 126, 0.77)"
 }
 else alert("current page underline not found")
+
+// fade stuff
+function checkIfVisible(target){
+    let eTop = target.offsetTop
+    let eBottom = eTop + target.offsetHeight // distancia hasta la parte superior mas la altura del elm.
+    
+    let screenTop = window.pageYOffset
+    let screenBottom = screenTop + window.innerHeight
+
+    return(eTop < screenBottom && eBottom > screenTop)
+}
+
+let fadies = document.getElementsByClassName("fadee")
+let expandies = document.getElementsByClassName("expand")
+
+window.addEventListener("scroll", () => {
+    for (let i of fadies){
+        if (checkIfVisible(i)){
+            i.style.opacity = "100%"
+            i.style.transform = "scale(100%)"
+        }
+    }
+
+    for (let i of expandies){
+        if (checkIfVisible(i)){
+            i.style.opacity = "100%"
+            i.style.transform = "scale(100%)"
+        }
+    }
+})
